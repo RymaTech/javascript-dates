86b30abcbee40abcdb19e1f4341cd2c1a4f913f5
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-07 21:33:25 +0100

    challenge complete

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,8 +1,29 @@
-import {format} from '../src/dateFormatter';
+import { format } from "../src/dateFormatter";
 
 it('when the system date and the date to format are the same day formats as "TODAY"', () => {
-  const December = 11; //js Date object month is indexed from 0
-  const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
-  const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
-  expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+  const december = 11; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2018, december, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2018, december, 15, 12, 50).getTime(); //Same day different time
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("TODAY");
+});
+
+it('when the system date and the date to format are NOT the same day formats as "11/12/2018"', () => {
+  const december = 11; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2018, december, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2018, december, 11, 12, 50).getTime();
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("11/12/2018");
+});
+
+it('when the system date and the date to format are NOT the same day formats as "11/12/2018"', () => {
+  const january = 0; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2018, january, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2018, january, 11, 12, 50).getTime();
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("11/1/2018");
+});
+
+it("console.log Error is arguments are not numbers", () => {
+  expect(format("11th December 2018", "11th December 2018")).toBe(
+    "Error: arguments must be numbers"
+  );
+  expect(format(true, false)).toBe("Error: arguments must be numbers");
 });
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,26 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  if (
+    typeof dateToFormatTimeMillis !== "number" ||
+    typeof systemDateTimeMillis !== "number"
+  ) {
+    return "Error: arguments must be numbers";
+  }
+
+  let dateToFormat = new Date(dateToFormatTimeMillis);
+  let formattedDate =
+    dateToFormat.getDate() +
+    "/" +
+    (dateToFormat.getMonth() + 1) +
+    "/" +
+    dateToFormat.getFullYear();
+
+  let systemDateToFormat = new Date(systemDateTimeMillis);
+  let formattedSystemDate =
+    systemDateToFormat.getDate() +
+    "/" +
+    (systemDateToFormat.getMonth() + 1) +
+    "/" +
+    systemDateToFormat.getFullYear();
+
+  return formattedDate === formattedSystemDate ? "TODAY" : formattedDate;
 };
