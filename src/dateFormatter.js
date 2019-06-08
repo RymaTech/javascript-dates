6d2135a3dc18364d4a687f8ee9eac9db845d0136
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-08 07:50:50 +0100

    refactored

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -6,21 +6,13 @@ export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
     return "Error: arguments must be numbers";
   }
 
-  let dateToFormat = new Date(dateToFormatTimeMillis);
-  let formattedDate =
-    dateToFormat.getDate() +
-    "/" +
-    (dateToFormat.getMonth() + 1) +
-    "/" +
-    dateToFormat.getFullYear();
+  const dateToFormat = new Date(dateToFormatTimeMillis);
+  const formattedDate = `${dateToFormat.getDate()}/${dateToFormat.getMonth() +
+    1}/${dateToFormat.getFullYear()}`;
 
-  let systemDateToFormat = new Date(systemDateTimeMillis);
-  let formattedSystemDate =
-    systemDateToFormat.getDate() +
-    "/" +
-    (systemDateToFormat.getMonth() + 1) +
-    "/" +
-    systemDateToFormat.getFullYear();
+  const systemDateToFormat = new Date(systemDateTimeMillis);
+  const formattedSystemDate = `${systemDateToFormat.getDate()}/${systemDateToFormat.getMonth() +
+    1}/${systemDateToFormat.getFullYear()}`;
 
   return formattedDate === formattedSystemDate ? "TODAY" : formattedDate;
 };
